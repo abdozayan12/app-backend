49be7dc737ac83cdf92a8f3e2545ad6a2c6ee080
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2023-11-04 19:52:26 +0200

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_04_174009) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_04_175130) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "greetings", force: :cascade do |t|
-    t.string "message"
+    t.string "greeting"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
